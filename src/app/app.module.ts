61218683e432fc10775d2000bd72e71c45ad658c
Pavel Nedzelskiy <pavel.nedzelskiy@dukascopy.com>
2017-08-10 15:14:54 +0300

    hero tour -> routing done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,29 @@
 import { NgModule }      from '@angular/core';
 import { FormsModule }   from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { HeroDetailComponent } from './hero-detail.component';
-
 import { AppComponent }  from './app.component';
+import { RouterModule }   from '@angular/router';
+import { HeroService }         from './hero.service';
+import { HeroesComponent }     from './heroes.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule }     from './app-routing.module';
+import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent }     from './dashboard.component';
 
 @NgModule({
-  imports:      [
-      BrowserModule,
-      FormsModule
-  ],
-  declarations: [
-      AppComponent,
-      HeroDetailComponent
-  ],
-  bootstrap:    [ AppComponent ]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        HeroDetailComponent,
+        DashboardComponent,
+        HeroesComponent
+    ],
+    providers: [
+        HeroService
+    ],
+    bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
